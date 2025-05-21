5f4b4ee7bf3d858d93c845d53affa2a1bb26d3d8
Gang <kingrkddn@gmail.com>
2025-05-21 18:02:42 +0900

    05.21
    whatthe

---
+++ Assets/02.Script/ProgrammerA.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace DevA
+{
+    public class ProgrammerA : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        int number1;
+
+        public int number2;
+
+        private int number3;
+
+        [SerializeField] int number4;
+
+        [SerializeField]
+        private int number5;
+
+        // Update is called once per frame
+
+    }
+}
---
+++ Assets/02.Script/ProgrammerB.cs
@@ -0,0 +1,24 @@
+using DevA;
+using UnityEngine;
+
+public class ProgrammerB : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public ProgrammerA pA;
+
+    void Start()
+    {
+        
+        //{
+        //    pA.number1 = 10;
+
+        //    pA.number2 = 20;
+
+        //    pA.number3 = 30;
+
+        //    pA.number4 = 40;
+
+        //    pA.number5 = 50;
+        //}
+    }
+}
